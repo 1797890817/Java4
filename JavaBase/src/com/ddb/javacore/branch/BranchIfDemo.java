ef716b650dfcee6c4d6b1ad18d5558f9813cd9c7
dbdu <1797890817@qq.com>
2017-03-22 14:18:07 +0800

    add for branch and DataType

---
+++ JavaBase/src/com/ddb/javacore/branch/BranchIfDemo.java
@@ -0,0 +1,60 @@
+package com.ddb.javacore.branch;
+
+import java.util.Scanner;
+
+public class BranchIfDemo {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入一个分数：");
+		int score = input.nextInt();
+
+		// 单分支if
+		if (score == 100) {
+			System.out.println("奖励你一个记事本！");
+		}
+
+		// 双分支
+		if (score >= 60) {
+			System.out.println("恭喜你，考试通过！");
+		} else {
+			System.out.println("考试不及格，请补考！");
+		}
+
+		// 多分支
+		if (score >= 90) {
+			System.out.println("非常好！");
+		} else if (score >= 80) {
+			System.out.println("良好！");
+		} else if (score >= 70) {
+			System.out.println("很好！");
+		} else if (score >= 60) {
+			System.out.println("好！");
+		} else if (score == 60) {
+			System.out.println("及格！");
+		} else if (score < 60) {
+			System.out.println("不及格！");
+		}
+
+		// switch
+		switch (score) {
+		case 90:
+			System.out.println("非常好！");
+			break;
+		case 80:
+			System.out.println("良好！");
+			break;
+		case 70:
+			System.out.println("很好！");
+			break;
+		case 60:
+			System.out.println("好！");
+			break;
+		default:
+			System.out.println("不及格！");
+			break;
+		}
+
+	}
+
+}
--- JavaBase/src/com/ddb/javacore/operator/DataTypeDemo.java
+++ JavaBase/src/com/ddb/javacore/operator/DataTypeDemo.java
@@ -6,7 +6,7 @@ public class DataTypeDemo {
 
 	public static void main(String[] args) {
 		PrintStream prt = System.out;
-		//基础运算符	+-/*
+		// 基础运算符 +-/*
 		int int1 = 99;
 		int int2 = 25;
 		prt.println("int1 + int2 = " + (int1 + int2));
@@ -14,16 +14,15 @@ public class DataTypeDemo {
 		prt.println("int1 * int2 = " + (int1 * int2));
 		prt.println("int1 / int2 = " + (int1 / int2));
 		prt.println("int1 % int2 = " + (int1 % int2));
-		
-		//int1 +=int2;
+
+		// int1 +=int2;
 		prt.println("--------------------------------");
-		prt.println("int1 +=int2 :" + (int1 +=int2));
-		prt.println("int1 -=int2 :" + (int1 -=int2));
-		prt.println("int1 *=int2 :" + (int1 *=int2));
-		prt.println("int1 /=int2 :" + (int1 /=int2));
-		prt.println("int1 %=int2 :" + (int1 %=int2));
-		
-		/*
+		prt.println("int1 +=int2 :" + (int1 += int2));
+		prt.println("int1 -=int2 :" + (int1 -= int2));
+		prt.println("int1 *=int2 :" + (int1 *= int2));
+		prt.println("int1 /=int2 :" + (int1 /= int2));
+		prt.println("int1 %=int2 :" + (int1 %= int2));
+
 		// 关系运算符
 		int a = 50;
 		int b = 60;
@@ -45,37 +44,43 @@ public class DataTypeDemo {
 		prt.println("(1<2) && (4<5) :" + ((1 < 2) && (4 < 5)));
 		prt.println("(1<2) || (4<5) :" + ((1 < 2) || (4 < 5)));
 
+		int aa = 5;// 定义一个变量；
+		boolean bb = (aa < 4) && (aa++ < 10);
+		System.out.println("使用短路逻辑运算符的结果为" + bb);
+		System.out.println("aa的结果为" + aa);
+		// 解析： 该程序使用到了短路逻辑运算符(&&)，首先判断 a<4 的结果为 false，则 b 的结果必定是 false，
+		// 所以不再执行第二个操作 a++<10 的判断，所以 a 的值为 5。
+
 		// 三目运算符
 		int i = 5, j = 9;
 		int result = i > j ? i : j; // ？号前面的表达式为真取i，否则取j
 		prt.println("result ： " + result);
 
-		
-		//String
+		// String
 		String address1 = "Anhui";
 		String address2 = "Hefei";
 		int code = 5;
-		String ret = address1 +" "+ address2 +" "+ code+" 号";
+		String ret = address1 + " " + address2 + " " + code + " 号";
 		prt.println("ret :" + ret);
-		
-		//类型转换
+
+		// 类型转换
 		int num = 1000000000;
 		short shot;
 		shot = (short) num;
 		prt.println("shot : " + shot);
-				
-		//递增递减运算符
+
+		// 递增递减运算符
 		int age = 6;
 		prt.println("age = " + age);
-		age++;	//先使用后++
+		age++; // 先使用后++
 		prt.println("age++: " + age);
 		age--;
 		prt.println("age--: " + age);
-		++age;	//先自增再使用
-		prt.println("++age: " + age);		
+		++age; // 先自增再使用
+		prt.println("++age: " + age);
 		--age;
-		prt.println("--age: " + age);		*/
-		
+		prt.println("--age: " + age);
+
 	}
 
 }
