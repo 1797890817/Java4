991a74ff2df1f71dbcb0f3037fca84ecde9ed815
dbdu <1797890817@qq.com>
2017-04-13 14:31:26 +0800

    add main thread demo

---
+++ JavaBase/src/com/ddb/javacore/thread/DemoMainThread.java
@@ -0,0 +1,46 @@
+package com.ddb.javacore.thread;
+
+import java.io.PrintStream;
+
+public class DemoMainThread {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) throws InterruptedException {
+		PrintStream out = System.out;
+		Thread thread = Thread.currentThread();
+		out.println("thread.getName() : " + thread.getName());
+		out.println("thread.getId() : " + thread.getId());
+		out.println("thread.getPriority() : " + thread.getPriority());
+		out.println("thread.isAlive() : " + thread.isAlive());
+		out.println("thread.isDaemon() : " + thread.isDaemon());
+		out.println("thread.isInterrupted() : " + thread.isInterrupted());
+		out.println("thread.getThreadGroup() : " + thread.getThreadGroup());
+		out.println("Thread.MAX_PRIORITY : " + Thread.MAX_PRIORITY);
+		out.println("Thread.MIN_PRIORITY : " + Thread.MIN_PRIORITY);
+		out.println("Thread.NORM_PRIORITY : " + Thread.NORM_PRIORITY);
+		out.println("将休眠5秒");
+		long start = System.currentTimeMillis();
+		out.println("当前时间的毫秒是：" + start);
+		Thread.sleep(5000);
+		// long end = ;
+		long end = System.currentTimeMillis();
+		out.println("休眠时间结束的毫秒是：" + end);
+		out.println("休眠5秒结束");
+		System.out.println("总计时间：" + (end - start));
+		out.println("即将调用：Thread.yield()");
+		thread.yield(); // 让出CPU
+		out.println("调用：Thread.yield()结束！");
+
+		// 实现Runnable接口的方式
+		Thread myRunable = new Thread(new MyRunnable());
+		myRunable.setName("MyRunable");
+		myRunable.setDaemon(true);	//设置为后台线程
+		myRunable.start();
+		Thread.sleep(2000);    //休眠2秒
+		System.out.println("myRunable.isDaemon():" + myRunable.isDaemon());
+		//修改为后台守护线程
+		
+		
+	}
+
+}
