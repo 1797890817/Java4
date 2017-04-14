858d87d7472791cf296596a7ecc3fd15df58ed44
dbdu <1797890817@qq.com>
2017-04-14 12:07:51 +0800

    add

--- JavaBase/src/com/ddb/javacore/thread/Account.java
+++ JavaBase/src/com/ddb/javacore/thread/Account.java
@@ -31,7 +31,6 @@ public class Account {
 			try {
 				System.out.println(Thread.currentThread().getName() + " 开始要执行wait操作" + " 执行了wait操作" + " -- i=" + i);
 				wait();
-				System.out.println(Thread.currentThread().getName() + " 执行了wait操作" + " 执行了wait操作" + " -- i=" + i);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -39,11 +38,12 @@ public class Account {
 			// 开始取钱
 			System.out.println(Thread.currentThread().getName() + " 取钱：" + drawAmount + " -- i=" + i);
 			setBalance(getBalance() - drawAmount);
-			System.out.println("取钱后余额为：" + getBalance());
+			System.out.println("***取钱后余额为：" + getBalance());
 			
 			hasMoney = false;
 			// 唤醒其他线程
-			notifyAll();
+			//notifyAll();
+			notify();
 			System.out.println(Thread.currentThread().getName() + "-- 取钱 -- 执行完毕" + " -- i=" + i); // 3
 		}
 	}
@@ -52,33 +52,32 @@ public class Account {
 	 * 存钱
 	 * @param depositeAmount
 	 */
-	public synchronized void deposite(double depositeAmount, int i) {
+	public synchronized void deposite(double depositeAmount, int j) {
 		
 		if (hasMoney) {
 			// 账户中已有人存钱进去，此时当前线程需要等待阻塞
 			try {
-				System.out.println(Thread.currentThread().getName() + " 开始要执行wait操作" + " -- i=" + i);
+				System.out.println(Thread.currentThread().getName() + " 开始要执行wait操作" + " -- j=" + j);
 				wait();
-				// 1
-				System.out.println(Thread.currentThread().getName() + " 执行了wait操作" + " -- i=" + i);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		} else {
 			// 开始存钱
-			System.out.println(Thread.currentThread().getName() + " 存款:" + depositeAmount + " -- i=" + i);
+			System.out.println(Thread.currentThread().getName() + " 存款:" + depositeAmount + " -- j=" + j);
 			setBalance(balance + depositeAmount);
-			System.out.println("存钱后余额为：" + getBalance());
+			System.out.println("###存钱后余额为：" + getBalance());
 			hasMoney = true;
 			// 唤醒其他线程
-			notifyAll();
+			//notifyAll();
+			notify();
 			// 2
 			/*try {
 				Thread.sleep(3000); 
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}*/
-			System.out.println(Thread.currentThread().getName() + "-- 存钱 -- 执行完毕" + " -- i=" + i);
+			System.out.println(Thread.currentThread().getName() + "-- 存钱 -- 执行完毕" + " -- j=" + j);
 		}
 	}
 
