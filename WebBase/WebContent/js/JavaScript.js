df8e897dfda8e1ca2bdea82106ce68f90c97762e
dbdu <1797890817@qq.com>
2017-04-26 14:35:31 +0800

    add for custom constructor

--- WebBase/WebContent/js/JavaScript.js
+++ WebBase/WebContent/js/JavaScript.js
@@ -11,8 +11,18 @@ function forIn() {
 	console.info("args :" + args);
 
 	var array = [ "AA", 22, true, "BB", "CC", 36.5 ];
-	for (var tmp in array) { // tmp是索引，不是元素值！
+	for ( var tmp in array) { // tmp是索引，不是元素值！
 		console.info(tmp + "---" + array[tmp]);
+	}
+
+	var myObject = new Object();
+	myObject.name = "James";
+	myObject.age = "22";
+	myObject.phone = "555 1234";
+	// 枚举（循环）对象的所有属性
+	for (var prop in myObject) { // 显示 "The property 'name' is James"，等等。
+		//window.alert("The property '" + prop + "' is " + myObject[prop]);
+		console.info(prop+"++++"+myObject[prop]);
 	}
 
 }
