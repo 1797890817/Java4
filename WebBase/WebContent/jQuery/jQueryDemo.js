59527ab15e9cc19eb54cebe7a39201ff84e2f83e
dbdu <1797890817@qq.com>
2017-04-28 16:01:40 +0800

    add for jQuery

---
+++ WebBase/WebContent/jQuery/jQueryDemo.js
@@ -0,0 +1,71 @@
+/**
+ * jQuery Demo
+ */
+/*
+ * function get_text() { var domObj = document.getElementById('txt');
+ * alert(document.getElementById('txt').value); }
+ */
+function get_text() {
+	var jq = $('#txt');
+	alert(jq.val());
+	// alert($('#txt').val());
+}
+
+// 标签选择器
+function divSelect() {
+	$("div").css('border', '1px solid red'); // 两个参数表示的是设置，一个参数表示是获取
+	alert($("div").css('border'));
+}
+
+// ID选择器
+function idSelect() {
+	$("#to").html($("#from").html()); // 有参数表示是设置，无参数表示是获取
+}
+// Class选择器
+function classSelect() {
+	$(".fruit").css('color', 'red');
+}
+
+// 组合选择器
+function mutiSelect() {
+	$(".fruit,#tomato").css('color', 'red');
+}
+
+// 祖先后代选择器
+function anceDescSelect() {
+	$("div label").css('color', 'red');
+}
+
+// 父子选择器
+function anceDescSelect2() {
+	// $("div > label").css('color', 'red');
+	$("p > label").css('color', 'red');
+}
+// 隔壁选择器
+function geBiSelect() { // 同级的紧紧相邻的
+	$("p + label").css('color', 'red');
+}
+
+// 邻居选择器
+function linjuSelect() { // 同级的都可以，可以不相邻
+	$("p ~ label").css('color', 'red');
+}
+
+// 过滤选择器 first -last
+function firstLastSelect() { // 不区分不同的ul，只取第一个最后一个
+	$('li:first').css('color', 'red');
+	$('li:last').css('color', 'blue');
+}
+// 过滤选择器 first -last-Child
+function firstLastChildSelect() { // 相对来说是一个整体，有多个ul时，都生效！
+	$('li:first-child').css('color', 'red');
+	$('li:last-child').css('color', 'blue');
+}
+
+// 指定下标的过滤选择器
+function indexSelect() {
+	$('li:eq(2)').css('color', 'red');
+}
+
+
+
