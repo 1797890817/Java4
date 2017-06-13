3c8aa82424e622b9688f17e1f6da1bbea0c969f5
dbdu <1797890817@qq.com>
2017-06-13 14:44:10 +0800

    aop

--- AspectjDemo/src/com/ddb/aspectj/demo/aop/AuthAspect.java
+++ AspectjDemo/src/com/ddb/aspectj/demo/aop/AuthAspect.java
@@ -11,9 +11,10 @@ package com.ddb.aspectj.demo.aop;
  */
 public aspect AuthAspect
 {
-	// 指定在执行org.crazyit.app.service包中任意类的、任意方法之前执行下面代码块
+	// 指定在执行com.ddb.aspectj.demo.service包中任意类的、任意方法之前执行下面代码块
 	// 第一个星号表示返回值不限；第二个星号表示类名不限；
-	// 第三个星号表示方法名不限；圆括号中..代表任意个数、类型不限的形参
+	// 第三个星号表示方法名不限；
+	// 圆括号中..代表任意个数、类型不限的形参
 	before(): execution(* com.ddb.aspectj.demo.service.*.*(..))
 	{
 		System.out.println("模拟进行权限检查...");
